141ac732a1c2c6ad95769fabd92cb586907d2563
ShaxbozMengziyoyev <shaxboz370@gmail.com>
2021-09-13 22:24:11 +0500

    commit

---
+++ javascript.js
@@ -0,0 +1,43 @@
+
+var firstName = prompt ("Ismigizni kiriting") .trim() .toUpperCase();
+var sureName = prompt ("familagizni kiriting") .trim() .toUpperCase();
+var age = prompt ("yoshigizni kiriting") .trim() .toUpperCase();
+var language = prompt ("Qaysi tillarni bilasiz") .trim() .toUpperCase();
+var work = prompt ("Avvaliri qayerda ishlagansiz") .trim() .toUpperCase();
+var salary = prompt ("Qancha oylik istaysiz") .trim() .toUpperCase();
+var work = prompt ("Javobini kuting") .trim() .toUpperCase();
+var goodbye =prompt ("Hayr salomat bo'ling") .trim() .toUpperCase();
+console.log(
+  `Ismigiz: ${firstName} familagizni kiriting: ${sureName} yoshigizni kiriting: ${age} Qaysi tillarni bilasiz: ${language} 
+  Avvaliri qayerda ishlagansiz: ${work} Qancha oylik istaysiz: ${salary} Javobini kuting: ${work} Hayr salomat bo'ling ${goodbye}`
+ );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var intro = alert ("salom ish");
+// var sureName = prompt ("familagizni kiriting")
+// var firstName = prompt ("Ismigizni kiriting");
+// var age = prompt ("yoshigizni kiriting");
+// // var camalCase = "Assalom, O'zbekiston";
+// // var numberVariable = 77;
+
+// console.log(
+//   `Ismigiz: ${firstName} familagizni kiriting: ${sureName} yoshigizni kiriting: ${age}`
+//   );
+// // console.log(firstName);
+// // console.log(sureName);
+// // console.log(age);
+// // console.log(camalCase); 
+// // console.log(numberVariable);
+// // console.log(intro);
